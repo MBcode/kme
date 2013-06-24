7bfc6933c0b985408354bdb7ae3d3eb34e72bf28
mike b <mike.bobak+git@gmail.com>
2013-06-24 16:36:56 -0700

    show output of each km fnc

--- test/test.lisp
+++ test/test.lisp
@@ -1,22 +1,29 @@
 (in)
-(load-kb "kme.km") ;"../kme.km"
-(sv-cls "inst1" "data")
-(sv "inst1" "val" 1)
-(taxonomy)
-;(showme 'km::|*inst1|)
-;or now:
-(show "inst1")
 
-;expect to see:
+(load-kb "kme.km") ;"../kme.km"
 ;Loading kme.km...
 ;kme.km loaded!
+
+(sv-cls "inst1" "data")
 ;ka:(*inst1 has (instance-of (data))),CONS
-;ka:(*inst1 has (val (1))),CONSRemoving redundant superclasses...
+
+(sv "inst1" "val" 1)
+;ka:(*inst1 has (val (1))),CONS
+
+(taxonomy)
+;Removing redundant superclasses...
 ;Removing redundant subclasses...
 ;Computing subclasses of Thing...
 ;Thing
 ;   data
 ;I     *inst1 
+
+ ;(showme 'km::|*inst1|)
+ ;or now:
+(show "inst1")
 ;(*inst1 has 
 ;  (instance-of (data))
 ;  (val (1))) 
+ 
+(print (gv "inst1" "val")) 
+;1
