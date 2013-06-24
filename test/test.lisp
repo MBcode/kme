5fea212ace46737de1bf847a6fe18bca791cf9bf
mike b <mike.bobak+git@gmail.com>
2013-06-24 14:39:21 -0700

    test construct then display

--- test/test.lisp
+++ test/test.lisp
@@ -1,11 +1,20 @@
 (in)
-(load-kb "../kme.km")
+(load-kb "kme.km") ;"../kme.km"
 (sv-cls "inst1" "data")
 (sv "inst1" "val" 1)
 (taxonomy)
 (showme 'km::|*inst1|)
 
 ;expect to see:
+;Loading kme.km...
+;kme.km loaded!
+;ka:(*inst1 has (instance-of (data))),CONS
+;ka:(*inst1 has (val (1))),CONSRemoving redundant superclasses...
+;Removing redundant subclasses...
+;Computing subclasses of Thing...
+;Thing
+;   data
+;I     *inst1 
 ;(*inst1 has 
 ;  (instance-of (data))
 ;  (val (1))) 
