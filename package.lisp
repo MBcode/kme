cf8df9fb17dd2a3b51f4de998fb8fe1170cda2c7
mike b <mike.bobak+git@gmail.com>
2013-06-28 13:16:15 -0700

    skip pkg.lsp 4now

--- package.lisp
+++ package.lisp
@@ -10,8 +10,16 @@
                  :taxonomy 
                  ;:show 
                  :showme :km :load-kb :all-instances :save-kb
+                 ;:PORT-TO-PACKAGE
                   ;:gvl :svl :pin :typ 
                 ;:eval-km :typ-p :is-a-p
                        :get-slotsvals))
 
+
+#+ignore
+(import 'km::(taxonomy show showme km load-kb all-instances save-kb
+                       get-slotsvals))
+;(km:port-to-package '(taxonomy show showme km load-kb all-instances save-kb get-slotsvals) :kme)
 (defun lo () (sb-ext:exit)) ;better one in my .sbclrc
+(defun bt (&optional (n 7)) (sb-debug:backtrace n))
+
